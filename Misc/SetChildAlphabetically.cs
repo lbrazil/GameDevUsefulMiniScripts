4e0fe91c256442141b869afc698894537d985af2
lbrazil <35836639+lbrazil@users.noreply.github.com>
2018-05-09 10:14:48 +0800

    Add files via upload

---
+++ Misc/SetChildAlphabetically.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetChildAlphabetically : MonoBehaviour {
+
+    [SerializeField] List<GameObject> content = new List<GameObject>();
+
+    private void Awake()
+    {
+        content.Sort((x, y) => x.name.CompareTo(y.name));
+        for(int i = 0; i < content.Count; i++)
+        {
+            content[i].transform.SetSiblingIndex(i);
+        }
+    }
+}
