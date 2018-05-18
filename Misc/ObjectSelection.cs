ec1a097e89a8116ac6f9cf7105b8f66cb019e53b
lbrazil <35836639+lbrazil@users.noreply.github.com>
2018-05-18 12:10:20 +0800

    object selection.
    switch prefab to selected object index using button

---
+++ Misc/ObjectSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ObjectSelection : MonoBehaviour {
+
+    public TextMeshProUGUI CurObjectName;
+
+    int furnitureIndex;
+    public List<GameObject> AvailableFurnitures = new List<GameObject>();
+
+    public void NextObject()
+    {
+        if(furnitureIndex < AvailableFurnitures.Count-1)
+        {
+            furnitureIndex++;
+        }
+        else
+        {
+            furnitureIndex = 0;
+        }
+        CurObjectName.text = AvailableFurnitures[furnitureIndex].gameObject.name;
+        GetComponent<CreateFurniture>().Object = AvailableFurnitures[furnitureIndex];
+    }
+}
